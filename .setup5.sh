4ed99cd5f9be44996bd93df230f96a49350976ac
Ali898989 <42411309+Ali898989@users.noreply.github.com>
2019-03-14 15:03:31 +0200

    Add files via upload

---
+++ .setup5.sh
@@ -0,0 +1,6 @@
+echo "pkg update " > /data/data/com.termux/files/home/payload5/.tool/metasploit/bmsf.sh
+echo "pkg upgrade " >> /data/data/com.termux/files/home/payload5/.tool/metasploit/bmsf.sh
+echo "pkg install unstable-repo " >> /data/data/com.termux/files/home/payload5/.tool/metasploit/bmsf.sh
+echo "pkg install metasploit " >> /data/data/com.termux/files/home/payload5/.tool/metasploit/bmsf.sh
+echo "msfconsole" >> /data/data/com.termux/files/home/payload5/.tool/metasploit/bmsf.sh
+payload
