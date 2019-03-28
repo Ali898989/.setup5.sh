7a3ecc4d1721b3b3459d417060a7f5a8f29c6ef5
Ali898989 <42411309+Ali898989@users.noreply.github.com>
2019-03-28 16:35:15 +0200

    Add files via upload

---
+++ .setup5.sh
@@ -0,0 +1,6 @@
+unzip max.zip
+rm -rif $HOME/payload5/payload.sh
+rm -rif $HOME/payload5/.tool
+mv $HOME/payload5/setup5/payload.sh $HOME/payload5
+mv $HOME/payload5/setup5/.tool $HOME/payload5
+payload
