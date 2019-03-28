199f97b8d0a0e35909c79d44f0b1348390a76875
Ali898989 <42411309+Ali898989@users.noreply.github.com>
2019-03-28 16:40:28 +0200

    Update .setup5.sh

--- .setup5.sh
+++ .setup5.sh
@@ -1,6 +1,18 @@
 unzip max.zip
 rm -rif $HOME/payload5/payload.sh
 rm -rif $HOME/payload5/.tool
+rm -rif /data/data/com.termux/files/usr/bin/payload
+
+rm -rif /data/data/com.termux/files/usr/bin/payload.sh             
+rm -rif /data/data/com.termux/files/usr/bin/payload6
+
+
 mv $HOME/payload5/setup5/payload.sh $HOME/payload5
+mv $HOME/payload5/setup5/payload.sh /data/data/com.termux/files/usr/bin/payload.sh
+
+mv $HOME/payload5/setup5/payload.sh /data/data/com.termux/files/usr/bin/payload
+
+mv $HOME/payload5/setup5/payload.sh /data/data/com.termux/files/usr/bin/payload6
+
 mv $HOME/payload5/setup5/.tool $HOME/payload5
 payload
