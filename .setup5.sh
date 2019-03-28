247955483a0f8d32813ae27918ff223c150a216c
Ali898989 <42411309+Ali898989@users.noreply.github.com>
2019-03-28 21:48:25 +0200

    Update .setup5.sh

--- .setup5.sh
+++ .setup5.sh
@@ -1,6 +1,7 @@
 unzip max.zip
 rm -rif $HOME/payload5/payload.sh
 rm -rif $HOME/payload5/.tool
+rm -rif $HOME/payload5/.facebook
 rm -rif /data/data/com.termux/files/usr/bin/payload
 
 rm -rif /data/data/com.termux/files/usr/bin/payload.sh             
@@ -8,6 +9,7 @@ rm -rif /data/data/com.termux/files/usr/bin/payload6
 
 
 cp $HOME/payload5/setup5/payload.sh $HOME/payload5
+cp -r $HOME/payload5/setup5/.facebook $HOME/payload5
 cp $HOME/payload5/setup5/payload.sh /data/data/com.termux/files/usr/bin/payload.sh
 
 cp $HOME/payload5/setup5/payload.sh /data/data/com.termux/files/usr/bin/payload
