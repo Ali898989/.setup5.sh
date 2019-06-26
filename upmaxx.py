f5d32ef6385bd2199f16e6c9059fc8a13f3547ae
Ali898989 <42411309+Ali898989@users.noreply.github.com>
2019-06-26 15:10:27 -0700

    Add files via upload

---
+++ upmaxx.py
@@ -0,0 +1,5 @@
+from os import system as a
+a('cd /sdcard && mkdir upmaxx')
+a('mv /data/data/com.termux/files/usr/roooot.zip /sdcard/upmaxx')
+a('cd /sdcard/upmaxx && unzip /sdcard/upmaxx/roooot.zip > hh.txt')
+a('exit')
